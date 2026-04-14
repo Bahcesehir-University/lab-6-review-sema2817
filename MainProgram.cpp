e0db0ec652c1b371177f07848b376ccbed1a222b
sema2817 <semaalmasri06@gmail.com>
2026-04-14 12:50:00 +0300

    Update MainProgram.cpp

--- MainProgram.cpp
+++ MainProgram.cpp
@@ -146,7 +146,7 @@ public:
     // Example: "Student(Ali, ID: 101, GPA: 3.5)"
     friend ostream& operator<<(ostream& os, const Student& s) {
         // YOUR CODE HERE
-        os << "Student(" << s.name << ", ID:"<<s.id<< ", GPA: "<< s.gpa <<")";
+        os << "Student(" << s.name << ", ID: "<<s.id<< ", GPA: "<< s.gpa <<")";
         return os ;
     }
 };
